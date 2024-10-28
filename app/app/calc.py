19bf0e18cf1223573858fa93a2a09323c6a00f7e
Lee <lmbrooksit@icloud.com>
2024-10-28 16:25:58 -0400

    made some changes to the app

--- app/app/calc.py
+++ app/app/calc.py
@@ -1,9 +1,8 @@
 # calculator functions
 
-def add(x, y):
-    # add x and y and return result.
+def add(x, y):  # add x and y and return result.
     return x + y
 
-def subtract(x, y):
-    #subtract x from y and return result.
+
+def subtract(x, y):  # subtract x from y and return result.
     return y - x
