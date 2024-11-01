6bed83a1468678cf3074211f7df119934ed32b3f
Lee <lmbrooksit@icloud.com>
2024-11-01 07:00:53 -0400

    added postgres

--- app/app/settings.py
+++ app/app/settings.py
@@ -76,10 +76,15 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'your_db_name',
+        'USER': 'your_user',
+        'PASSWORD': 'your_password',
+        'HOST': '127.0.0.1',  # or '127.0.0.1'
+        'PORT': '',  # default is 5432
     }
 }
+
 
 
 # Password validation
