7950ac598bf9e3023086a6bfadd6bb2af4fd7912
Lee <lmbrooksit@icloud.com>
2024-11-01 13:53:02 -0400

    updated dockerfile

--- app/app/settings.py
+++ app/app/settings.py
@@ -76,15 +76,10 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'datagurudatabase',
-        'USER': 'admin',
-        'PASSWORD': 'Bsu2019!',
-        'HOST': '127.0.0.1',  # or '127.0.0.1'
-        'PORT': '5432',  # default is 5432
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 
 # Password validation
